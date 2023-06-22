c11cbf861d81114c2433343bca976db7c390cec9
aymane330 <aymanefilali330@gmail.com>
2023-06-22 05:53:38 +0100

    fix pwd + fix cd

--- execution/src/builtin/pwd.c
+++ execution/src/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: ayel-fil <ayel-fil@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 03:49:51 by ayel-fil          #+#    #+#             */
-/*   Updated: 2023/06/22 03:27:26 by ayel-fil         ###   ########.fr       */
+/*   Updated: 2023/06/22 05:50:33 by ayel-fil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ int	execute_pwd(t_env **env)
 	path = getcwd(NULL, 0);
 	if (!path)
 	{
-		ft_putstr_fd("path not found\n", STDERR_FILENO);
-		return (1);
+		path = get_value("PWD", *env);
+		ft_putstr_fd(path, OUT);
+		ft_putstr_fd("\n", OUT);
+		return (0);
 	}
 	path = get_value("PWD", *env);
 	ft_putstr_fd(path, OUT);
